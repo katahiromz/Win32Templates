a5188fde5509a75a531a8f268d9321ae505130e0
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-03-27 14:24:55 +0900

    improve Canvas

--- PanelApp/Canvas.c
+++ PanelApp/Canvas.c
@@ -1,10 +1,12 @@
 #include "stdafx.h"
 
-#define CLASS_NAME TEXT("Canvas")
+#define CLASS_NAME TEXT("STATIC")
 
 extern HINSTANCE g_hInstance;
 extern HWND g_hCanvasWnd;
 extern INT g_iActivePanel;
+
+static WNDPROC g_fnOldWndProc = NULL;
 
 static BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
 {
@@ -28,6 +30,13 @@ static void OnDrawClient(HWND hwnd, HDC hdc)
     LineTo(hdc, rc.left, rc.bottom);
 }
 
+static BOOL OnEraseBkgnd(HWND hwnd, HDC hdc)
+{
+    RECT rc;
+    GetClientRect(hwnd, &rc);
+    FillRect(hdc, &rc, GetStockBrush(DKGRAY_BRUSH));
+}
+
 static void OnPaint(HWND hwnd)
 {
     HDC hdc;
@@ -41,15 +50,27 @@ static void OnPaint(HWND hwnd)
     }
 }
 
+static void OnSize(HWND hwnd, UINT state, int cx, int cy)
+{
+    InvalidateRect(hwnd, NULL, TRUE);
+}
+
 static LRESULT CALLBACK
 WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
         HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
+#if 1
+        HANDLE_MSG(hwnd, WM_ERASEBKGND, OnEraseBkgnd);
         HANDLE_MSG(hwnd, WM_PAINT, OnPaint);
+#endif
+        HANDLE_MSG(hwnd, WM_SIZE, OnSize);
     default:
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        if (g_fnOldWndProc)
+            return CallWindowProc(g_fnOldWndProc, hwnd, uMsg, wParam, lParam);
+        else
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
     return 0;
 }
@@ -59,30 +80,35 @@ HWND createCanvasWnd(HWND hwnd)
     HWND hCanvasWnd;
     DWORD style, exstyle;
 
-    style = WS_CHILD | WS_VISIBLE;
+    style = WS_CHILD | WS_VISIBLE | SS_BITMAP;
     exstyle = WS_EX_CLIENTEDGE;
     hCanvasWnd = CreateWindowEx(exstyle, CLASS_NAME, NULL, style, 0, 0, 0, 0,
         hwnd, NULL, g_hInstance, NULL);
+
+    g_fnOldWndProc = SubclassWindow(hCanvasWnd, WindowProc);
     return hCanvasWnd;
 }
 
 BOOL registerCanvas(HINSTANCE hInst)
 {
-    WNDCLASSEX wcx = { sizeof(wcx), CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS };
-    wcx.lpfnWndProc = WindowProc;
-    wcx.hInstance = g_hInstance;
-    //wcx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    //wcx.hIcon = LoadIcon(g_hInstance, MAKEINTRESOURCE(IDI_MAIN));
-    wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
-    //wcx.hbrBackground = (HBRUSH)(COLOR_3DFACE + 1);
-    wcx.hbrBackground = (HBRUSH)(COLOR_3DDKSHADOW + 1);
-    //wcx.lpszMenuName = MAKEINTRESOURCE(IDR_MAINMENU);
-    wcx.lpszClassName = CLASS_NAME;
-    wcx.hIconSm = NULL;
-    if (!RegisterClassEx(&wcx))
+    if (0)
     {
-        MessageBox(NULL, LoadStringDx(IDS_FAILREGCLASS), NULL, MB_ICONERROR);
-        return FALSE;
+        WNDCLASSEX wcx = { sizeof(wcx), CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS };
+        wcx.lpfnWndProc = WindowProc;
+        wcx.hInstance = g_hInstance;
+        //wcx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+        //wcx.hIcon = LoadIcon(g_hInstance, MAKEINTRESOURCE(IDI_MAIN));
+        wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
+        //wcx.hbrBackground = (HBRUSH)(COLOR_3DFACE + 1);
+        wcx.hbrBackground = (HBRUSH)(COLOR_3DDKSHADOW + 1);
+        //wcx.lpszMenuName = MAKEINTRESOURCE(IDR_MAINMENU);
+        wcx.lpszClassName = CLASS_NAME;
+        wcx.hIconSm = NULL;
+        if (!RegisterClassEx(&wcx))
+        {
+            MessageBox(NULL, LoadStringDx(IDS_FAILREGCLASS), NULL, MB_ICONERROR);
+            return FALSE;
+        }
     }
 
     return TRUE;
